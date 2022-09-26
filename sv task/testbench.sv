f98af9e58ac3ab1d9a1b6ab12c730c259fa8555e
01fe21mve007 <103169114+01fe21mve007@users.noreply.github.com>
2022-09-26 15:23:18 +0530

    Add files via upload

---
+++ sv task/testbench.sv
@@ -0,0 +1,14 @@
+
+module sv_task;
+  int x;
+  task sum(input int a,b,output int c);
+    c = a+b;  
+  endtask
+   initial  begin
+    sum(10,5,x);
+    $display("\tValue of x = %0d",x);
+  end
+endmodule
+
+
+
